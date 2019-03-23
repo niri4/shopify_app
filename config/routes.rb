2d31f1e5db37cc6b50a64fd3a0dde2b7c8b3c4f3
niri4 <nirankarsingh247@gmail.com>
2019-03-24 03:21:22 +0530

    add different views in home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  root :to => 'home#index'
+
   mount ShopifyApp::Engine, at: '/'
+  get 'modal' => "home#modal", :as => :modal
+ get 'modal_buttons' => "home#modal_buttons", :as => :modal_buttons
+ get 'regular_app_page' => "home#regular_app_page"
+ get 'help' => "home#help"
+ get 'pagination' => "home#pagination"
+ get 'breadcrumbs' => "home#breadcrumbs"
+ get 'buttons' => "home#buttons"
+ get 'form_page' => "home#form_page"
+ post 'form_page' => "home#form_page"
+ get 'error' => 'home#error'
+ root :to => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
